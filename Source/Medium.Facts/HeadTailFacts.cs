3fccde53515753d1908d05ac259ada89a121d6b4
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-07-05 01:57:33 +0700

    Move

--- Source/Medium.Facts/HeadTailFacts.cs
+++ Source/Medium.Facts/HeadTailFacts.cs
@@ -1,11 +1,26 @@
-using Medium;
 using System.Collections.Generic;
 using Xunit;
+using System.Linq;
+using System;
+
+ public static class SeqExtensions {
+    public static void Deconstruct<T>(this IEnumerable<T> seq, out T head, out IEnumerable<T> tail) {
+        head = seq.FirstOrDefault();
+        tail = new List<T>(seq.Take(1));
+    }
+}
 
 public class HeadTailFacts {
     [Fact]
     public void T() {
-        var list = new List<int> { 1,2,3,4,5 };
-        var (h,t) = list;
+
+        var list = new List<string> { "a", "a", "a", "b", "c", "c", "a", "a", "d", "e", "e", "e", "e" };
+        var compress = list.Aggregate(Enumerable.Empty<string>(), (a, x) =>  
+            a.LastOrDefault() == x ? a : a.Concat(new [] { x }));
+        Assert.Equal(new List<string> { "a", "b", "c", "a", "d", "e" }, compress);
+
+        var result2 = list.Aggregate(Enumerable.Empty<string>(), (a, x) =>  
+            a.LastOrDefault() == x ? a : a.Concat(new List<string> { x }));
+        Assert.Equal(new List<string> {"a", "b", "c", "a", "d", "e" }, result2);
     }
 }
