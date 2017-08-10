e5c3d02e3e0bf0320ec7eb2b188ea78aebc75dff
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-08-10 21:01:07 +0700

    Sum

---
+++ Scripts/Sum/FSharp.fsx
@@ -0,0 +1,10 @@
+let rec comb n l =
+  match (n,l) with
+  | (0,_) -> [[]]
+  | (_,[]) -> []
+  | (n,x :: xs) ->
+      let useX = List.map (fun l -> x :: l) (comb (n - 1) xs)
+      let noX = comb n xs
+      useX @ noX
+
+[5;5;6;8;8;12;8;10] |> comb 5 |> List.filter (List.sum >> (=) 40) |> printfn "%A"
