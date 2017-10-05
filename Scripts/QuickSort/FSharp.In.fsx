4e20a5e7fe3fa9f106b8183ffbb58e1f9e7d4a56
wk-j <somnuk.wk@gmail.com>
2017-10-06 02:07:48 +0700

    In

---
+++ Scripts/QuickSort/FSharp.In.fsx
@@ -0,0 +1,11 @@
+let filter = List.filter
+let rec quick = function
+    | [] -> []
+    | x :: xs ->
+        let p1 = xs |> filter (fun e -> e < x)  |> quick 
+        let p2 = xs |> filter (fun e -> e >= x) |> quick
+        in p1 @ [x] @ p2
+
+[1;99;2;98;3;97;4;96;4;95;5;94]
+|> quick
+|> printfn "%A"
