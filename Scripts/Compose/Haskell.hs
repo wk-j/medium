51858212d1d111d0b71176deabd469f05dfca4d5
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-11-15 11:56:35 +0700

    Compose

--- Scripts/Compose/Haskell.hs
+++ Scripts/Compose/Haskell.hs
@@ -1,13 +1,18 @@
 -- stack runghc --package strict
 
 import Control.Category hiding ((.))
+import Data.List
 
 add1 = (+1)
 add2 = (+2)
 
-add3 = add1 >>> add2
+add3 = add1 <<< add2
 add3' = add1 . add2
 
 main = do
     print $ add3 5
     print $ add3' 5
+
+    print $ sort . tail $ [5, 4, 3, 2, 1]
+    print $ sort <<< tail $ [5, 4, 3, 2, 1]
+    print $ tail >>> sort $ [5, 4, 3, 2, 1]
