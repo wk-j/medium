811e64de2e172d641c6da8dd5e55c2e69c5238c9
Somnuk Wongkhan <somnuk.wk@gmail.com>
2018-01-18 23:28:34 +0700

    Initializer

---
+++ Scripts/Initializer/Init.csx
@@ -0,0 +1,17 @@
+
+
+public static class ListExtensions {
+    public static void Add<T>(this List<T> list, int size, T defaultValue) {
+        list.AddRange(Enumerable.Repeat(defaultValue, size));
+    }
+}
+
+var list = new List<int> {
+    { 10, 100 },
+    { 10, 200 },
+    { 10, 300 }
+};
+
+WriteLine(list.Where(x => x == 100).Count() == 10);
+WriteLine(list.Where(x => x == 200).Count() == 10);
+WriteLine(list.Where(x => x == 300).Count() == 10);
---
+++ Source/Initializer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Initializer {
+
+    public static class ListExtensions {
+        public static void Add<T>(this List<T> list, int size, T defaultValue) {
+            list.AddRange(Enumerable.Repeat(defaultValue, size));
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            var list = new List<int> {
+                { 10, 100 },
+                { 10, 200 },
+                { 10, 300 }
+            };
+
+            Console.WriteLine(list.Where(x => x == 100).Count() == 10);
+            Console.WriteLine(list.Where(x => x == 200).Count() == 10);
+            Console.WriteLine(list.Where(x => x == 300).Count() == 10);
+        }
+    }
+
+}
