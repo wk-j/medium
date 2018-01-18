2507d21aae11b415a594b2fd86cd7533f746ea67
Somnuk Wongkhan <somnuk.wk@gmail.com>
2018-01-19 02:16:35 +0700

    Update initializer

--- Source/Initializer/Program.cs
+++ Source/Initializer/Program.cs
@@ -18,11 +18,23 @@ namespace Initializer {
 
     class Program {
         static void Main(string[] args) {
+
             var list = new List<int> {
                 { 10, 100 },
                 { 10, 200 },
-                { 10, 300 }
+                { 10, 300 },
+                1,2,3,4
             };
+
+            var array = new[] {
+                1,2,3,4,4,6
+            };
+
+            var dict = new Dictionary<string, int> {
+                { "A", 100 },
+                { "B", 200 }
+            };
+
 
             Console.WriteLine(list.Where(x => x == 100).Count() == 10);
             Console.WriteLine(list.Where(x => x == 200).Count() == 10);
